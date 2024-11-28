143df35a5d9fbecc063ab09a2b710e8519680dda
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 00:50:25 +0700

    feat: create TokenUtil file for implement jwt method

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/utils/TokenUtil.kt
@@ -0,0 +1,57 @@
+package com.zuraa.blog_api_spring.utils
+
+import com.zuraa.blog_api_spring.entity.User
+import com.zuraa.blog_api_spring.auth.jwt.JwtProperties
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.Date
+
+@Component
+class TokenUtil(
+    private val jwtProperties: JwtProperties
+) {
+    private val secretKey = Keys.hmacShaKeyFor(
+        jwtProperties.key.toByteArray()
+    )
+
+    fun generate(
+        userDetails: User,
+        expirationDate: Date,
+        additionalClaims: Map<String, Any> = emptyMap()
+    ): String =
+        Jwts.builder()
+            .claims()
+            .subject(userDetails.email)
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(expirationDate)
+            .add(additionalClaims)
+            .and()
+            .signWith(secretKey)
+            .compact()
+
+    fun isValid(token: String, userDetails: UserDetails): Boolean {
+        val email = extractEmail(token)
+        return userDetails.username == email && !isExpired(token)
+    }
+
+    fun extractEmail(token: String): String? =
+        getAllClaims(token)
+            .subject
+
+    fun isExpired(token: String): Boolean =
+        getAllClaims(token)
+            .expiration
+            .before(Date(System.currentTimeMillis()))
+
+    private fun getAllClaims(token: String): Claims {
+        val parser = Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+        return parser
+            .parseSignedClaims(token)
+            .payload
+    }
+}
