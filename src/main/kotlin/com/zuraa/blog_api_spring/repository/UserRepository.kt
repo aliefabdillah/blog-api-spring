bfbe21ba585bfb73c8a5a1403099e1838641706f
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 00:44:32 +0700

    chore: add nullable into  parameter

--- src/main/kotlin/com/zuraa/blog_api_spring/repository/UserRepository.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/repository/UserRepository.kt
@@ -3,8 +3,9 @@ package com.zuraa.blog_api_spring.repository
 import com.zuraa.blog_api_spring.entity.User
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
+import java.util.Optional
 
 interface UserRepository : MongoRepository<User, String> {
     @Query("{'email': ?0}")
-    fun findByEmail(email: String): User?
+    fun findByEmail(email: String?): User?
 }
