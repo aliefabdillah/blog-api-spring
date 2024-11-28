82a5b42f95f468f3860c6be4f179b60b07f4b8ca
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 19:28:46 +0700

    chore: change type of status properties

--- src/main/kotlin/com/zuraa/blog_api_spring/model/Response.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/model/Response.kt
@@ -1,13 +1,15 @@
 package com.zuraa.blog_api_spring.model
 
+import org.springframework.http.HttpStatus
+
 data class ApiSuccessResponse<T>(
     val data: T? = null,
-    val status: String,
+    val status: HttpStatus,
     val code: Int
 )
 
 data class ApiErrorResponse (
     val message: String,
-    val status: String,
+    val status: HttpStatus,
     val code: Int
 )
