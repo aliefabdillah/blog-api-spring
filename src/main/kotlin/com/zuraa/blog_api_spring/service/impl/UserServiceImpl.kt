d6a0dbec518d25588ce49a009ca1b0e32a843b93
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 00:44:11 +0700

    chore: cleaning code

--- src/main/kotlin/com/zuraa/blog_api_spring/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/impl/UserServiceImpl.kt
@@ -1,14 +1,11 @@
 package com.zuraa.blog_api_spring.service.impl
 
-import com.mongodb.client.model.Collation
 import com.zuraa.blog_api_spring.entity.User
 import com.zuraa.blog_api_spring.model.ApiSuccessResponse
 import com.zuraa.blog_api_spring.model.UserRegisterRequest
 import com.zuraa.blog_api_spring.repository.UserRepository
 import com.zuraa.blog_api_spring.service.UserService
 import com.zuraa.blog_api_spring.utils.HashUtil
-import org.springframework.data.mongodb.core.query.Criteria
-import org.springframework.data.mongodb.core.query.Query
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
@@ -17,7 +14,6 @@ import java.util.Date
 @Service
 class UserServiceImpl(val userRepository: UserRepository, val hashUtil: HashUtil) : UserService {
     override fun create(request: UserRegisterRequest): ApiSuccessResponse<User> {
-
         val existingUser = userRepository.findByEmail(request.email)
         if (existingUser != null) {
             throw ResponseStatusException(HttpStatus.CONFLICT, "Email already register", null)
