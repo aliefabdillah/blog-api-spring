962bfdaa71d6ab13e42593c249357ed999ebaea9
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 00:45:34 +0700

    fix: add anotation to make data class JwtProperties can use as constructor

--- src/main/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplication.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplication.kt
@@ -1,9 +1,12 @@
 package com.zuraa.blog_api_spring
 
+import com.zuraa.blog_api_spring.auth.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 class BlogApiSpringApplication
 
 fun main(args: Array<String>) {
