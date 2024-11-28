facf025a82d2412f9bf46be3a099c223719ef9c0
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 01:33:40 +0700

    feat: call validate util in register service

--- src/main/kotlin/com/zuraa/blog_api_spring/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/impl/UserServiceImpl.kt
@@ -6,20 +6,27 @@ import com.zuraa.blog_api_spring.model.UserRegisterRequest
 import com.zuraa.blog_api_spring.repository.UserRepository
 import com.zuraa.blog_api_spring.service.UserService
 import com.zuraa.blog_api_spring.utils.HashUtil
+import com.zuraa.blog_api_spring.utils.ValidationUtil
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
 import java.util.Date
 
 @Service
-class UserServiceImpl(val userRepository: UserRepository, val hashUtil: HashUtil) : UserService {
+class UserServiceImpl(val userRepository: UserRepository, val hashUtil: HashUtil, val validationUtil: ValidationUtil) : UserService {
     override fun create(request: UserRegisterRequest): ApiSuccessResponse<User> {
+        // VALIDATE INPUT
+        validationUtil.validate(request)
+
+        //  CHECK EXISTING USER
         val existingUser = userRepository.findByEmail(request.email)
         if (existingUser != null) {
             throw ResponseStatusException(HttpStatus.CONFLICT, "Email already register", null)
         }
 
+        // CRYPTO PASSWORD
         val hashedPassword = hashUtil.hashBcrypt(request.password)
+
 
         val userRequest = User(
             name = request.name,
@@ -29,6 +36,7 @@ class UserServiceImpl(val userRepository: UserRepository, val hashUtil: HashUtil
             updatedAt = Date()
         )
 
+        // SAVE TO DATABASE
         val usersCreated = userRepository.save(userRequest)
 
         return ApiSuccessResponse(data = usersCreated, code = 201, status = HttpStatus.CREATED)
