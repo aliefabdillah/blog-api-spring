4baf21652572a6d030a68db94669085fe273f843
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 18:35:12 +0700

    init: create starter project

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.zuraa.blog_api_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogApiSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApiSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zuraa.blog_api_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApiSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
