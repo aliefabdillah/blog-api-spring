cf70e58b0511f157ccf5b371106e360fbc7f9fad
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 21:52:34 +0700

    fix: typo parameter

--- src/main/kotlin/com/zuraa/blog_api_spring/entity/User.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/entity/User.kt
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.util.Date
 import java.util.UUID
 
-@Document(collation = "users")
+@Document(collection = "users")
 data class User(
     @Id
     val id: String = UUID.randomUUID().toString(),
