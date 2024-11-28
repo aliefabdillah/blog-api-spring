ab86b974589a00e9bbce5bbe384706b4e24ca83b
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 01:34:02 +0700

    chore: update annotation validation for user model

--- src/main/kotlin/com/zuraa/blog_api_spring/model/Request.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/model/Request.kt
@@ -1,7 +1,17 @@
 package com.zuraa.blog_api_spring.model
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class UserRegisterRequest(
+    @field:NotBlank
     val name: String,
+
+    @field:NotBlank
+    @field:Email
     val email: String,
+
+    @field:Size(min = 6, message = "Password at least must be 8 char long")
     val password: String,
 )
