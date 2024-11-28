02545d5033d10f8aaf035d418ff7d0cab2b94ea0
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 21:53:31 +0700

    fix: create register endpoint

--- src/main/kotlin/com/zuraa/blog_api_spring/controller/UserController.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/controller/UserController.kt
@@ -1,11 +1,22 @@
 package com.zuraa.blog_api_spring.controller
 
+import com.zuraa.blog_api_spring.entity.User
+import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.UserRegisterRequest
 import com.zuraa.blog_api_spring.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/user")
 class UserController(val userService: UserService) {
 
+    @PostMapping(value = ["/register"], produces = ["application/json"], consumes = ["application/json"])
+    fun registerUser(@RequestBody body: UserRegisterRequest): ApiSuccessResponse<User> {
+        return userService.create(request = body)
+    }
 }
