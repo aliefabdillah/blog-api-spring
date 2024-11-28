b1f558b10bfbcd567e6a512666be52a685e5b618
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 19:04:44 +0700

    feat: create users model collection

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/entity/User.kt
@@ -0,0 +1,24 @@
+package com.zuraa.blog_api_spring.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.Date
+import java.util.UUID
+
+@Document(collation = "users")
+data class User(
+    @Id
+    val id: String = UUID.randomUUID().toString(),
+
+    val name: String,
+
+    @Indexed(unique = true)
+    val email: String,
+
+    val password: String,
+
+    val createdAt: Date,
+
+    val updatedAt: Date,
+)
