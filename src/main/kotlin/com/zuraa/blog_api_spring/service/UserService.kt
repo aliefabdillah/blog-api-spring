b5d6eb1e28e6b8e33c43e7888a50a923cc846182
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-28 19:15:42 +0700

    feat: create interface contract for user service

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
@@ -0,0 +1,9 @@
+package com.zuraa.blog_api_spring.service
+
+import com.zuraa.blog_api_spring.entity.User
+import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.UserRegisterRequest
+
+interface UserService {
+    fun create(request: UserRegisterRequest): ApiSuccessResponse<User>
+}
