8e9ebf589497c235ce822c176cbca402bb7024ac
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 00:44:57 +0700

    feat: create handle responseStatusException controller

--- src/main/kotlin/com/zuraa/blog_api_spring/controller/ErrorController.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/controller/ErrorController.kt
@@ -2,9 +2,11 @@ package com.zuraa.blog_api_spring.controller
 
 import com.zuraa.blog_api_spring.model.ApiErrorResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.client.HttpClientErrorException.Unauthorized
+import org.springframework.web.server.ResponseStatusException
 
 @RestControllerAdvice
 class ErrorController {
@@ -12,5 +14,10 @@ class ErrorController {
     @ExceptionHandler(value = [Unauthorized::class])
     fun unauthorized(): ApiErrorResponse {
         return ApiErrorResponse("Login first before access this endpoint", HttpStatus.UNAUTHORIZED, code = 401)
+    }
+
+    @ExceptionHandler(ResponseStatusException::class)
+    fun handleResponseStatusException(exception: ResponseStatusException): ResponseEntity<Any> {
+        return ResponseEntity(exception.body, exception.statusCode)
     }
 }
