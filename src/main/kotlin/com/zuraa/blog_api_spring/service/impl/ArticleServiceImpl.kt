603affe8c21b4f2cdf1004784ed4c560591e9f3f
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 16:32:34 +0700

    feat: create article service file

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/impl/ArticleServiceImpl.kt
@@ -0,0 +1,29 @@
+package com.zuraa.blog_api_spring.service.impl
+
+import com.zuraa.blog_api_spring.entity.Article
+import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.CreateArticleRequest
+import com.zuraa.blog_api_spring.model.UpdateArticleRequest
+import com.zuraa.blog_api_spring.service.ArticleService
+
+class ArticleServiceImpl : ArticleService {
+    override fun create(createRequest: CreateArticleRequest): ApiSuccessResponse<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAll(query: Any): ApiSuccessResponse<List<Article>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getById(id: String): ApiSuccessResponse<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(id: String, updateRequest: UpdateArticleRequest): ApiSuccessResponse<Article> {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: String): ApiSuccessResponse<Any> {
+        TODO("Not yet implemented")
+    }
+}
