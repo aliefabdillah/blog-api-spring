65492654339edeaa9eb8a53444f675964ee18bda
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 11:23:38 +0700

    feat: create contract for getAuthUser method

--- src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
@@ -5,8 +5,10 @@ import com.zuraa.blog_api_spring.model.ApiSuccessResponse
 import com.zuraa.blog_api_spring.model.UserLoginRequest
 import com.zuraa.blog_api_spring.model.UserPublicResponse
 import com.zuraa.blog_api_spring.model.UserRegisterRequest
+import org.springframework.security.core.Authentication
 
 interface UserService {
     fun create(request: UserRegisterRequest): ApiSuccessResponse<UserPublicResponse>
     fun auth(request: UserLoginRequest): ApiSuccessResponse<Any>
+    fun getUserAuth(auth: Authentication): ApiSuccessResponse<UserPublicResponse>
 }
