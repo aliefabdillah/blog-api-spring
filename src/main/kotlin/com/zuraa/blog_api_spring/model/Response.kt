836a989a57e58ff1a19971b4bb47a464c8d88666
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-29 10:39:03 +0700

    feat: create response public response

--- src/main/kotlin/com/zuraa/blog_api_spring/model/Response.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/model/Response.kt
@@ -1,6 +1,8 @@
 package com.zuraa.blog_api_spring.model
 
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.http.HttpStatus
+import java.util.*
 
 data class ApiSuccessResponse<T>(
     val data: T? = null,
@@ -8,8 +10,16 @@ data class ApiSuccessResponse<T>(
     val code: Int
 )
 
-data class ApiErrorResponse (
+data class ApiErrorResponse(
     val message: String,
     val status: HttpStatus,
     val code: Int
+)
+
+data class UserPublicResponse(
+    val id: String,
+    val name: String,
+    val email: String,
+    val createdAt: Date,
+    val updatedAt: Date,
 )
