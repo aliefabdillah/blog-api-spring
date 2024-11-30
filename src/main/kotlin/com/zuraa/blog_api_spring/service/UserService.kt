94b28762db1e6a6cb0d5d37c1f4b89709b97b0f6
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 20:27:28 +0700

    feat: create new update profile method contract

--- src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
@@ -2,10 +2,16 @@ package com.zuraa.blog_api_spring.service
 
 import com.zuraa.blog_api_spring.model.*
 import org.springframework.security.core.Authentication
+import org.springframework.web.multipart.MultipartFile
 
 interface UserService {
     fun create(request: UserRegisterRequest): ApiSuccessResponse<UserAuthPublicResponse>
     fun auth(request: UserLoginRequest): ApiSuccessResponse<Any>
     fun getUserAuth(auth: Authentication): ApiSuccessResponse<UserAuthPublicResponse>
     fun getUserWithArticle(auth: Authentication): ApiSuccessResponse<UserWithArticle>
+    fun update(
+        auth: Authentication,
+        request: UpdateProfileRequest,
+        files: MultipartFile
+    ): ApiSuccessResponse<UserAuthPublicResponse>
 }
