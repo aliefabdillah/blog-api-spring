d7041d36010ad5ae100c6091823a1fbe98e1f105
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 14:20:55 +0700

    feat: create new method contract

--- src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/UserService.kt
@@ -1,14 +1,11 @@
 package com.zuraa.blog_api_spring.service
 
-import com.zuraa.blog_api_spring.entity.User
-import com.zuraa.blog_api_spring.model.ApiSuccessResponse
-import com.zuraa.blog_api_spring.model.UserLoginRequest
-import com.zuraa.blog_api_spring.model.UserPublicResponse
-import com.zuraa.blog_api_spring.model.UserRegisterRequest
+import com.zuraa.blog_api_spring.model.*
 import org.springframework.security.core.Authentication
 
 interface UserService {
-    fun create(request: UserRegisterRequest): ApiSuccessResponse<UserPublicResponse>
+    fun create(request: UserRegisterRequest): ApiSuccessResponse<UserAuthPublicResponse>
     fun auth(request: UserLoginRequest): ApiSuccessResponse<Any>
-    fun getUserAuth(auth: Authentication): ApiSuccessResponse<UserPublicResponse>
+    fun getUserAuth(auth: Authentication): ApiSuccessResponse<UserAuthPublicResponse>
+    fun getUserWithArticle(auth: Authentication): ApiSuccessResponse<UserWithArticle>
 }
