785c8efbfd8db4f88e18d16415ac4088621c6055
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 11:03:14 +0700

    feat: add create article endpoint

--- src/main/kotlin/com/zuraa/blog_api_spring/controller/ArticleController.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/controller/ArticleController.kt
@@ -1,11 +1,22 @@
 package com.zuraa.blog_api_spring.controller
 
+import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.ArticleWithAuthor
+import com.zuraa.blog_api_spring.model.CreateArticleRequest
 import com.zuraa.blog_api_spring.service.ArticleService
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/article")
 class ArticleController(val articleService: ArticleService) {
 
+    @PostMapping(value = ["/api/article/"])
+    fun createArticle(
+        authentication: Authentication,
+        @RequestBody createArticleRequest: CreateArticleRequest
+    ): ApiSuccessResponse<ArticleWithAuthor> {
+        return articleService.create(authentication, createArticleRequest)
+    }
 }
