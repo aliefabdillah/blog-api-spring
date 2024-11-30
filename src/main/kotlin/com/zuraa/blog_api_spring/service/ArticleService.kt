676a9e8b51670caa346c3e6032372a9e7e3a09d4
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 11:01:02 +0700

    chore: add new parameter

--- src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
@@ -2,11 +2,13 @@ package com.zuraa.blog_api_spring.service
 
 import com.zuraa.blog_api_spring.entity.Article
 import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.ArticleWithAuthorResponse
 import com.zuraa.blog_api_spring.model.CreateArticleRequest
 import com.zuraa.blog_api_spring.model.UpdateArticleRequest
+import org.springframework.security.core.Authentication
 
 interface ArticleService {
-    fun create(createRequest: CreateArticleRequest): ApiSuccessResponse<Article>
+    fun create(auth: Authentication, createRequest: CreateArticleRequest): ApiSuccessResponse<ArticleWithAuthorResponse>
     fun getAll(query: Any): ApiSuccessResponse<List<Article>>
     fun getById(id: String): ApiSuccessResponse<Article>
     fun update(id: String, updateRequest: UpdateArticleRequest): ApiSuccessResponse<Article>
