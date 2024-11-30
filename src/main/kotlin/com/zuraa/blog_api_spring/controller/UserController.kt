cf85c8c1cd76317edd9c08fdb395bf0a1db1a465
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 20:28:04 +0700

    feat: create new update profile api endpoint

--- src/main/kotlin/com/zuraa/blog_api_spring/controller/UserController.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/controller/UserController.kt
@@ -4,10 +4,14 @@ import com.zuraa.blog_api_spring.model.*
 import com.zuraa.blog_api_spring.service.UserService
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/api/user")
@@ -31,5 +35,14 @@ class UserController(val userService: UserService) {
     @GetMapping(value = ["/me/details"])
     fun getAuthUserWithRelation(auth: Authentication): ApiSuccessResponse<UserWithArticle> {
         return userService.getUserWithArticle(auth)
+    }
+
+    @PatchMapping(value = ["/me"])
+    fun updateProfile(
+        auth: Authentication,
+        @RequestParam("files") files: MultipartFile,
+        @ModelAttribute formData: UpdateProfileRequest
+    ): ApiSuccessResponse<UserAuthPublicResponse> {
+        return userService.update(auth = auth, files = files, request = formData)
     }
 }
