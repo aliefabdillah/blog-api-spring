ed9bffc69bfe8a1844fa5d02d0704e966caa7f8f
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 20:43:49 +0700

    chore: add multipart files to method parameter

--- src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
@@ -3,11 +3,12 @@ package com.zuraa.blog_api_spring.service
 import com.zuraa.blog_api_spring.entity.Article
 import com.zuraa.blog_api_spring.model.*
 import org.springframework.security.core.Authentication
+import org.springframework.web.multipart.MultipartFile
 
 interface ArticleService {
     fun create(auth: Authentication, createRequest: CreateArticleRequest): ApiSuccessResponse<ArticleWithAuthor>
     fun getAll(query: ListArticleQuery): ApiSuccessResponse<List<ArticleWithAuthor>>
     fun getById(id: String): ApiSuccessResponse<ArticleWithAuthor>
-    fun update(id: String, updateRequest: UpdateArticleRequest): ApiSuccessResponse<Article>
+    fun update(id: String, files: MultipartFile, updateRequest: UpdateArticleRequest): ApiSuccessResponse<Article>
     fun delete(id: String): ApiSuccessResponse<Any>
 }
