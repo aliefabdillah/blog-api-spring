935b43af260c23301f86416f707a351c2477b658
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 11:27:25 +0700

    feat: add get article by id endpoint

--- src/main/kotlin/com/zuraa/blog_api_spring/controller/ArticleController.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/controller/ArticleController.kt
@@ -5,18 +5,29 @@ import com.zuraa.blog_api_spring.model.ArticleWithAuthor
 import com.zuraa.blog_api_spring.model.CreateArticleRequest
 import com.zuraa.blog_api_spring.service.ArticleService
 import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/article")
 class ArticleController(val articleService: ArticleService) {
 
-    @PostMapping(value = ["/api/article/"])
+    @PostMapping(value = ["/"])
     fun createArticle(
         authentication: Authentication,
         @RequestBody createArticleRequest: CreateArticleRequest
     ): ApiSuccessResponse<ArticleWithAuthor> {
         return articleService.create(authentication, createArticleRequest)
+    }
+
+    @GetMapping(value = ["/{id}"])
+    fun getArticleById(
+        @PathVariable("id") id: String
+    ): ApiSuccessResponse<ArticleWithAuthor> {
+        return articleService.getById(id)
     }
 }
