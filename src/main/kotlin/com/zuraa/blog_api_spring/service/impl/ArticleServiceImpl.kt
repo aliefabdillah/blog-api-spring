7f6b428ae0599905e3b1482c01b786dfddeb72b6
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 11:03:01 +0700

    feat: add create article service logic

--- src/main/kotlin/com/zuraa/blog_api_spring/service/impl/ArticleServiceImpl.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/impl/ArticleServiceImpl.kt
@@ -2,15 +2,61 @@ package com.zuraa.blog_api_spring.service.impl
 
 import com.zuraa.blog_api_spring.entity.Article
 import com.zuraa.blog_api_spring.model.ApiSuccessResponse
+import com.zuraa.blog_api_spring.model.ArticleWithAuthor
 import com.zuraa.blog_api_spring.model.CreateArticleRequest
 import com.zuraa.blog_api_spring.model.UpdateArticleRequest
+import com.zuraa.blog_api_spring.repository.ArticleRepository
+import com.zuraa.blog_api_spring.repository.UserRepository
 import com.zuraa.blog_api_spring.service.ArticleService
+import com.zuraa.blog_api_spring.utils.ValidationUtil
+import com.zuraa.blog_api_spring.utils.toUserPublicResponse
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import java.util.Date
 
 @Service
-class ArticleServiceImpl : ArticleService {
-    override fun create(createRequest: CreateArticleRequest): ApiSuccessResponse<Article> {
-        TODO("Not yet implemented")
+class ArticleServiceImpl(
+    val validationUtil: ValidationUtil,
+    val userRepository: UserRepository,
+    val articleRepository: ArticleRepository
+) : ArticleService {
+    override fun create(auth: Authentication, createRequest: CreateArticleRequest): ApiSuccessResponse<ArticleWithAuthor> {
+        validationUtil.validate(createRequest)
+
+        // get user
+        val authUser =
+            userRepository.findByEmail(auth.name) ?: throw ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "User not login/authenticated"
+            )
+
+        val article = Article(
+            title = createRequest.title,
+            content = createRequest.content,
+            authorId = authUser.id,
+            createdAt = Date(),
+            updatedAt = Date()
+        )
+
+        articleRepository.save(article)
+
+        authUser.articles.add(article)
+
+        userRepository.save(authUser)
+
+        val articleResponse = ArticleWithAuthor(
+            id = article.id,
+            title = article.title,
+            content = article.content,
+            authorId = authUser.id,
+            author = authUser.toUserPublicResponse(),
+            createdAt = article.createdAt,
+            updatedAt = article.updatedAt
+        )
+
+        return ApiSuccessResponse(data = articleResponse, status = HttpStatus.CREATED, code = 201)
     }
 
     override fun getAll(query: Any): ApiSuccessResponse<List<Article>> {
