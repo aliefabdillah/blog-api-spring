80e29c0fe1122db5afc0af93af4b63a02cc0f944
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 14:20:37 +0700

    feat: create new extension function User

--- src/main/kotlin/com/zuraa/blog_api_spring/utils/UserExt.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/utils/UserExt.kt
@@ -1,13 +1,24 @@
 package com.zuraa.blog_api_spring.utils
 
 import com.zuraa.blog_api_spring.entity.User
-import com.zuraa.blog_api_spring.model.UserPublicResponse
+import com.zuraa.blog_api_spring.model.UserAuthPublicResponse
+import com.zuraa.blog_api_spring.model.UserWithArticle
 
-fun User.toUserPublicResponse(): UserPublicResponse =
-    UserPublicResponse(
+fun User.toUserPublicResponse(): UserAuthPublicResponse =
+    UserAuthPublicResponse(
         id = this.id,
         name = this.name,
         email = this.email,
         createdAt = this.createdAt,
         updatedAt = this.updatedAt
+    )
+
+fun User.toUserWithRelationResponse(): UserWithArticle =
+    UserWithArticle(
+        id = this.id,
+        name = this.name,
+        email = this.email,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
+        articles = this.articles
     )
