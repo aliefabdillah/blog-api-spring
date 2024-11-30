e41b1644b973c9b5ffff3f49d97e696e10c1e4d8
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 11:02:14 +0700

    chore: change return type name

--- src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/service/ArticleService.kt
@@ -2,13 +2,13 @@ package com.zuraa.blog_api_spring.service
 
 import com.zuraa.blog_api_spring.entity.Article
 import com.zuraa.blog_api_spring.model.ApiSuccessResponse
-import com.zuraa.blog_api_spring.model.ArticleWithAuthorResponse
+import com.zuraa.blog_api_spring.model.ArticleWithAuthor
 import com.zuraa.blog_api_spring.model.CreateArticleRequest
 import com.zuraa.blog_api_spring.model.UpdateArticleRequest
 import org.springframework.security.core.Authentication
 
 interface ArticleService {
-    fun create(auth: Authentication, createRequest: CreateArticleRequest): ApiSuccessResponse<ArticleWithAuthorResponse>
+    fun create(auth: Authentication, createRequest: CreateArticleRequest): ApiSuccessResponse<ArticleWithAuthor>
     fun getAll(query: Any): ApiSuccessResponse<List<Article>>
     fun getById(id: String): ApiSuccessResponse<Article>
     fun update(id: String, updateRequest: UpdateArticleRequest): ApiSuccessResponse<Article>
