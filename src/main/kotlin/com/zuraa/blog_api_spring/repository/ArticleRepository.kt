822357a225f7d18576bc7726388d74af99b27a2b
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-30 13:19:55 +0700

    feat: create findByTittleAndAuthorId method

--- src/main/kotlin/com/zuraa/blog_api_spring/repository/ArticleRepository.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/repository/ArticleRepository.kt
@@ -1,7 +1,14 @@
 package com.zuraa.blog_api_spring.repository
 
 import com.zuraa.blog_api_spring.entity.Article
+import com.zuraa.blog_api_spring.entity.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 
 interface ArticleRepository : MongoRepository<Article, String> {
+    @Query("{ \$and: [ { 'title': { \$regex: ?0, \$options: 'i' } }, { 'authorId': { \$in: ?1 } } ] }")
+    fun findByTitleAndAuthorId(title: String, authorIds: List<String>, pageable: Pageable): Page<Article>
+
 }
