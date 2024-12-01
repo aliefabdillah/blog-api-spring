6c2f6e31afad52d951b1771eeb8bc8b74cc516d0
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-01 21:44:04 +0700

    fix: create redis config

---
+++ src/main/kotlin/com/zuraa/blog_api_spring/config/RedisConfig.kt
@@ -0,0 +1,59 @@
+package com.zuraa.blog_api_spring.config
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.zuraa.blog_api_spring.model.ArticleWithAuthor
+import org.springframework.cache.CacheManager
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+class RedisConfig {
+    //    FUNCTION FOR CONFIG REDIS
+    @Bean
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): CacheManager {
+        val redisCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10))
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    StringRedisSerializer()
+                )
+            )
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfig)
+            .build()
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.connectionFactory = redisConnectionFactory
+
+        // Set serializers for the RedisTemplate
+        val stringSerializer = StringRedisSerializer()
+        val jsonSerializer = Jackson2JsonRedisSerializer(Any::class.java)
+
+        redisTemplate.keySerializer = stringSerializer
+        redisTemplate.valueSerializer = jsonSerializer
+        redisTemplate.hashKeySerializer = stringSerializer
+        redisTemplate.hashValueSerializer = jsonSerializer
+
+        return redisTemplate
+    }
+}
