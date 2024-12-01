84f5d98737f4650a15e456cd026f4f66071e8f34
alief abdillah <aliefmabdillah09@gmail.com>
2024-12-01 21:45:19 +0700

    feat: add annotation for enable caching

--- src/main/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplication.kt
+++ src/main/kotlin/com/zuraa/blog_api_spring/BlogApiSpringApplication.kt
@@ -4,9 +4,11 @@ import com.zuraa.blog_api_spring.auth.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties::class)
+@EnableCaching 	//annotation to make app can use cache
 class BlogApiSpringApplication
 
 fun main(args: Array<String>) {
